05959234df764d7bc44d835e42dc625b67107d12
peterzd <peterzd@gmail.com>
2013-07-28 23:18:32 +0800

    init repo, gem install redis

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'http://ruby.taobao.org'
+
+ruby '2.0.0'
+gem 'rails', '3.2.13'
+
+gem 'redis'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedisStudy::Application.config.secret_token = '170de234fb0500b612ea2075075c1d7df52d49ccee99982b1e95c9c99d4d4ee4a6f803ae07dcf2f65f5920421b6af1a8081648a8c3aa6eddb6f0f0e302ec4671'
